d7ab09b060a47cf5c87aac9836fd30b0b238ad88
giath <giath@172.19.124.112>
2020-08-05 15:47:43 +1000

    Robot can now be controlled with LCD keypad Button in basics directions

--- Setup.m
+++ Setup.m
@@ -1,9 +1,15 @@
+clear
+clf
+global robotPosition
+port = serialportlist("available")
+SerialPort = "COM4"; %% Change to the port connected to the Arduino
+BaudRate = 9600;   %% Communication baud rate
+robotPosition = transl(0,0,0.1);  % robot initial position is (0,0)
 
-%% Load floor 
+%% Load floor
 floorPose=transl(0,0,0);
 [fFloor,vFloor,dataFloor] = plyread('floor.ply','tri');
 floorVertexColours = [dataFloor.vertex.red,dataFloor.vertex.green,dataFloor.vertex.blue] / 255;
-hold on;       
 floor_h = trisurf(fFloor,vFloor(:,1),vFloor(:,2),vFloor(:,3),'FaceVertexCData',floorVertexColours,'EdgeColor','interp','EdgeLighting','flat');
 floorVertexCount = size(vFloor,1);
 updatedFloorPos = [floorPose * [vFloor,ones(floorVertexCount,1)]']';
@@ -12,32 +18,61 @@ drawnow();
 hold on
 
 %% Load Robot
-robotPosition = transl(0,0,0.1);  % robot initial position is (0,0)
-robot_heading = 0;                % original robot heading is x-axis(theta = 0)
 [f,v,data] = plyread('iRobot.ply','tri');
 VertexCount = size(v,1);
-VertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;   
+VertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
 MidPoint = sum(v)/VertexCount;
 Verts= v - repmat(MidPoint,VertexCount,1);
 %Plot the robot
 robot_h = trisurf(f,v(:,1),v(:,2),v(:,3),'FaceVertexCData',VertexColours,'EdgeColor','interp','EdgeLighting','flat');
 %Assigning position
-updatedBottom = [robotPosition * [Verts,ones(VertexCount,1)]']';
+updatedPosition = [robotPosition * [Verts,ones(VertexCount,1)]']';
 %Update the vertices
-robot_h.Vertices = updatedBottom(:,1:3);
+robot_h.Vertices = updatedPosition(:,1:3);
 axis auto equal
 
+%%Initialize communnicationusing global value in a function MATLAB
+arduinoObj = serialport(SerialPort,BaudRate);
+configureTerminator(arduinoObj,"CR/LF");
+flush(arduinoObj);
+
+while 1
+    %% how to communicate with arduinno ?
+    buffer = arduinoObj.readline;
+    
+    switch buffer
+        case "UP"
+            CMD_MOVE(0.05);
+        case "DOWN"
+            CMD_MOVE(-0.05);
+        case "RIGHT"
+            CMD_ROTATE(1,5);
+        case "LEFT"
+            CMD_ROTATE(0,5);
+    end
+    
+    updatedPosition = [robotPosition*[v,ones(size(v,1),1)]']';
+    robot_h.Vertices = updatedPosition(:,1:3);
+    drawnow()
+    
+    %% read input
+    %% execute
+    
+end
 
 
-
-%% Move Robot 
-function Tmatrix = CMD_MOVE(distance)
-Tmatrix = transl(distance,0,0)
-
+%% Move Robot
+function  CMD_MOVE(distance)
+global robotPosition
+robotPosition = robotPosition * transl(distance,0,0);
 end
 
 %% Rotate Robot
-%angle value POSITIVE is CCW, NEGATIVE is CCW
-function Tmatrix = CMD_ROTATE(angle)
-Tmatrix = trotz()
+%direction: 0 is CCW, 1 is CCW
+function CMD_ROTATE(direction,angle)
+if direction == 1
+    angle = -angle;
+end
+global robotPosition
+robotPosition = robotPosition * trotz(angle,'deg');
 end
